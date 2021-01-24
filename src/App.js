1e03fea7e12df2f2d01b17b2413f1f3ba3e0c766
Isfar Oshir <iao233@nyu.edu>
2021-01-23 23:24:09 -0500

    added router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom'
+import {Route, 
+        Switch,
+} from 'react-router-dom'
+
+import HomePage from './components/pages/HomePage/HomePage'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h1>Bye</h1>
+      <Router>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/friends" exact>
+          
+        </Route>
+      </Router>
     </div>
   );
 }
