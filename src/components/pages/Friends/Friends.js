8fafbffeb20a249c612602ce8dfc0a7d937d10ed
Farhan Mashud <fm1539@nyu.edu>
2021-01-28 13:07:47 -0500

    additional hub changes

--- src/components/pages/Friends/Friends.js
+++ src/components/pages/Friends/Friends.js
@@ -2,21 +2,37 @@ import React, {useState, useEffect} from 'react'
 import PlatNavbar from '../../Navbar'
 
 function Friends() {
+
+    let [friendState, setFriend] = useState('friends')
+
+    let friendHandler = (value) => {
+        setFriend(value)
+    }
+
+    let friendsDiv = <div className="container">
+        <h2>Isfar</h2>
+        <hr></hr>
+        <h2>Farhan</h2>
+    </div>
     return (
         <div>
             <PlatNavbar />
             <h3>Friends Hub</h3>
             <div className="tab-container">
                 <div className="tab">
-                    <h2>Friends</h2>
+                    <button style={{width: 100 }} onClick={() => friendHandler('friends')}>Friends</button>
                 </div>
                 <div className="tab">
-                    <h2>Find Friends</h2>
+                    <button style={{width: 100}} onClick={() => friendHandler('find')}>Find Friends</button>
                 </div>
                 <div className="tab">
-                    <h2>Requests</h2>
+                    <button style={{width: 100}} onClick={() => friendHandler('requests')}>Requests</button>
                 </div>
             </div>
+            {
+                friendState == 'friends' ? friendsDiv : <div></div>
+            }
+            
         </div>
     )
 }
