7dcfc7f47530c4ed8ba5ddf02e0ead1d3f7fbe05
Farhan Mashud <fm1539@nyu.edu>
2021-01-29 11:26:08 -0500

    new friends hub changes

--- src/components/pages/Friends/Friends.js
+++ src/components/pages/Friends/Friends.js
@@ -14,23 +14,51 @@ function Friends() {
         <hr></hr>
         <h2>Farhan</h2>
     </div>
+
+    let findDiv = <div className="container">
+        <h2>Oshir</h2>
+        <hr></hr>
+        <h2>Mashud</h2>
+    </div>
+
+    let friendStatus = "friends-btn"
+    let findStatus = "friends-btn"
+    let reqStatus = "friends-btn"
+
+    if (friendState==="friends") {
+        friendStatus = "friends-btn active"
+        findStatus = "friends-btn"
+        reqStatus = "friends-btn"
+    }
+    else if (friendState === "find") {
+        friendStatus = "friends-btn"
+        findStatus = "friends-btn active"
+        reqStatus = "friends-btn"
+    }
+    else {
+        friendStatus = "friends-btn"
+        findStatus = "friends-btn"
+        reqStatus = "friends-btn active"
+    }
+
     return (
         <div>
             <PlatNavbar />
             <h3>Friends Hub</h3>
             <div className="tab-container">
                 <div className="tab">
-                    <button style={{width: 100 }} onClick={() => friendHandler('friends')}>Friends</button>
+                    <button className={friendStatus} onClick={() => friendHandler('friends')}><h1 className="h1">Friends</h1></button>
                 </div>
                 <div className="tab">
-                    <button style={{width: 100}} onClick={() => friendHandler('find')}>Find Friends</button>
+                    <button className={findStatus} onClick={() => friendHandler('find')}><h1 className="h1">Find Friends</h1></button>
                 </div>
                 <div className="tab">
-                    <button style={{width: 100}} onClick={() => friendHandler('requests')}>Requests</button>
+                    <button className={reqStatus} onClick={() => friendHandler('requests')}><h1 className="h1">Requests</h1></button>
                 </div>
             </div>
             {
-                friendState == 'friends' ? friendsDiv : <div></div>
+                friendState == 'friends' ? friendsDiv : (friendState == 'find' ? findDiv : <div>
+                </div>)
             }
             
         </div>
